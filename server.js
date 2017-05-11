75f1104979a839c7aedefe7ef784e8a525aa6d36
roni4444 <debapriyopaul.dp@gmail.com>
2017-05-11 22:02:11 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname,'ui','index.html'));
+});
+
+app.get('/article-one',function(req,res){
+  res.send('Article one requested and served here');
+});
+
+app.get('/article-two',function(req,res){
+  res.send('Article two requested and served here');
+});
+
+app.get('/article-three',function(req,res){
+  res.send('Article three requested and served here');
+});
+
+app.get('/ui/style.css',function(req,res) {
+  res.sendFile(path.join(__dirname,'ui','style.css'));
+});
+
+app.get('/ui/madi.png',function(req,res) {
+  res.sendFile(path.join(__dirname,'ui','madi.png'));
+});
+
+var port = 8080;
+app.listen(8080, function(){
+  console.log('app listening on port ${port} ! ');
+});
